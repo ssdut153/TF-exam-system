07f8472313cb5a34eba326f26da51eda3b126b1d
志庄 杨 <yzz0427@outlook.com>
2016-06-08 16:13:29 +0800

    除追加外基本全部完成

--- TF_question_exam_system/JsonHelper.h
+++ TF_question_exam_system/JsonHelper.h
@@ -4,19 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "cJSON.h"
+#include "cJSON.h"//cJSON头文件，处理json数据
 
-int question_num = 0;
-struct Question
+int question_num = 0;//题库已有题数
+struct Question//题库结构体
 {
 	int number;
 	char question[1000];
 	int answer;
 };
 
-struct Question* questions;
+struct Question* questions;//题库数组
+
 // answer should be 1(true) or 0(false)
-char* BuildJson(int* number, char** quest, int* answer, int num)
+char* BuildJson(int* number, char** quest, int* answer, int num)//输入分别为 题号数组，题干数组，答案数组，总题数；返回对应格式的json字符串，答案只能为0或1
 {
 	int i = 0;
 	cJSON *root, **dir;
@@ -26,6 +27,7 @@ char* BuildJson(int* number, char** quest, int* answer, int num)
 	root = cJSON_CreateArray();
 	for (i = 0; i < num; i++)
 	{
+		//创建对象至数组
 		cJSON_AddItemToArray(root, dir[i] = cJSON_CreateObject());
 		//为对象添加字符串键值对
 		cJSON_AddNumberToObject(dir[i], "number", number[i]);
@@ -34,34 +36,35 @@ char* BuildJson(int* number, char** quest, int* answer, int num)
 	}
 	//将json结构体转换为字符串
 	out = cJSON_PrintUnformatted(root);
-	//out = cJSON_Print(root);
-	//删除
+	//释放内存
 	cJSON_Delete(root);
 	return out;
 }
 
-void WriteToFile(char* filename, char* Json)
+void WriteToFile(char* filename, char* Json)//写到文件
 {
 	FILE *fp;
 	if ((fp = fopen(filename, "w")) == NULL)
 	{
-		printf("\nerror on open/create file:%s\n", filename);
+		printf("\nerror on open/create file:%s\n\n", filename);
 		return;
 	}
 	else
 	{
 		fprintf(fp, "%s", Json);
 		fclose(fp);
+		printf("保存成功\n");
 	}
 }
 
-char* ReadFromFile(char* filename)
+char* ReadFromFile(char* filename)//从文件读取
 {
 	FILE *fp;
 	char read[1000000];
 	if ((fp = fopen(filename, "r")) == NULL)
 	{
 		printf("\nerror on open/create file:%s\n", filename);
+		printf("可能文件不存在，若首次运行本程序请忽略该报错\n\n");
 		return "";
 	}
 	else
@@ -72,12 +75,12 @@ char* ReadFromFile(char* filename)
 	}
 }
 
-void CleanFile(char* filename)
+void CleanFile(char* filename)//清空文件，本程序没用到
 {
 	FILE *fp;
 	if ((fp = fopen(filename, "w")) == NULL)
 	{
-		printf("\nerror on clean file:%s\n", filename);
+		printf("\nerror on clean file:%s\n\n", filename);
 		return;
 	}
 	else
@@ -86,18 +89,18 @@ void CleanFile(char* filename)
 	}
 }
 
-void ImportJson(char* pJson)
+void ImportJson(char* pJson)//解析json导入结构体数组
 {
 	cJSON * root = NULL, *item = NULL, *number, *question, *answer;
 	char *pr = NULL;
 	if (NULL == pJson)
 	{
-		printf("无数据！\n");
+		printf("无数据！\n\n");
 		return;
 	}
 	if ((root = cJSON_Parse(pJson)) == NULL)
 	{
-		printf("无法解析该json数据！\n");
+		printf("无法解析该json数据！\n\n");
 		return;
 	}
 	free(questions);
@@ -122,18 +125,20 @@ void ImportJson(char* pJson)
 		questions[iCnt].answer = atoi(cJSON_Print(answer));
 	}
 	cJSON_Delete(root);
+	printf("Json数据导入完成，题库共有%d道题\n\n",question_num);
 	return;
 }
 
-void AddQuestion(char* question, int answer)
+void AddQuestion(char* question, int answer)//添加问题到数组
 {
 	strcpy(questions[question_num].question, question);
 	questions[question_num].answer = answer;
 	questions[question_num].number = question_num + 1;
 	question_num++;
+	printf("录入成功\n");
 }
 
-void SaveToFile(char* filename)
+void SaveToFile(char* filename)//保存至文件
 {
 	int i = 0;
 	int* number = (int*)malloc(sizeof(int)*question_num);
@@ -150,12 +155,12 @@ void SaveToFile(char* filename)
 	WriteToFile(filename, BuildJson(number, quest, answer, question_num));
 }
 
-void LoadFromFile(char* filename)
+void LoadFromFile(char* filename)//从文件载入
 {
 	ImportJson(ReadFromFile(filename));
 }
 
-void PrintAll()
+void PrintAll()//全部输出
 {
 	int i = 0;
 	for (i = 0; i < question_num; i++)
--- TF_question_exam_system/main.c
+++ TF_question_exam_system/main.c
@@ -8,17 +8,147 @@
 
 	抽取的试题不能重复
 	检查输入合法性，异常处理
+
+	已知问题：
+		1.反复保存时题干的Json会被加多次\"
+		2.录入新题不支持空格
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #include "JsonHelper.h"
 #define FILENAME "questions.dat"
-int main(int argc, const char * argv[]) 
+
+int num = 0;//记录组题的总数
+int* index;//记录组题的题号
+int answer = 0;//记录组好的题是否被答过了，0未答，1已答
+int isExist(int* array, int value, int num)//判断数组中是否已有value值，防止组题重复
 {
-	LoadFromFile(FILENAME);
+	int i = 0;
+	for (; i < num; i++)
+		if (array[i] == value)
+			return 1;
+	return 0;
+}
+int* GetRandomIndex(int* index, int num)//取随机题号加入index数组
+{
+	int finish_num = 0;
+	int k;
+	srand(time(NULL));
+	while (finish_num < num)
+	{
+		//k = x + rand() % (y - x + 1) 可生成[x,y]范围内的随机数
+		//这里为了避免无法取到下表为0的情况生成[1,num]的随机数
+		k = 1 + rand() % (num + 1);
+		if (isExist(index, k, num))
+			continue;
+		index[finish_num++] = k;
+	}
+}
+void option1()//处理录入新题功能，不支持空格
+{
+	char question[1000];
+	int answer = 0;
+	printf("目前题库已有%d题,现在录入第%d道题\n", question_num, question_num + 1);
+	printf("请输入题干（不支持空格）：");
+	scanf("%s", question);
+	printf("请输入答案，正确输入1，错误输入0：");
+	scanf("%d", &answer);
+	while (answer != 1 && answer != 0)
+	{
+		printf("输入有误，正确输入1，错误输入0，请重新输入：");
+		scanf("%d", &answer);
+	}
+	AddQuestion(question, answer);//添加问题到结构体数组
+	SaveToFile(FILENAME);//以Json格式保存至文件
+}
+void option2()//处理输出功能
+{
 	PrintAll();
-	AddQuestion("Is caomai smart?", 0);
-	PrintAll();
-	SaveToFile(FILENAME);
+	printf("全部%d道题输出完毕\n", question_num);
+}
+void option3()//追加功能，具体功能不清楚，没有制作
+{
+	printf("功能尚未制作\n");
+}
+void option4()//组题功能
+{
+	int i = 0;
+	printf("请输入要抽取的题目数(不大于%d)：", question_num);
+	scanf("%d", &num);
+	if (num<1 || num>question_num)//判断输入题数是否为负数、0或大于题库数
+	{
+		printf("输入有误，请输入1至%d的数：", question_num);
+		scanf("%d", &num);
+	}
+	index = (int*)malloc(sizeof(int)*num);
+	GetRandomIndex(index, num);//随机取题库
+	printf("组题结果(输出为题号)：\n");
+	for (i = 0; i < num; i++)
+		printf("%d\t", index[i]);
+	printf("\n");
+}
+void option5()//答题功能
+{
+	int i = 0;
+	int this_ans = 0;//记录本题的作答
+	int right = 0;//记录答对个数
+	if (num == 0 || answer == 1)//判断是否已组题、组题是否被答过一遍
+		option4();
+	for (; i < num; i++)
+	{
+		printf("\n\n第%d题（%d/%d）\n", i + 1, i + 1, num);
+		printf("%s\n正确请输入1，错误请输入0，请答题：", questions[index[i] - 1].question);
+		scanf("%d", &this_ans);
+		while (this_ans != 0 && this_ans != 1)
+		{
+			printf("输入错误，正确请输入1，错误请输入0，请答题：");
+			scanf("%d", &this_ans);
+		}
+		if (this_ans == questions[index[i] - 1].answer)//判断答案是否正确
+			right++;
+	}
+	printf("\n\n答题结束，答对%d道，准确率：%5.2lf%%\n", right, 100.0*right / (double)num);
+	answer = 1;
+}
+void error()//处理1-5之外的输入
+{
+	printf("输入错误，请重新输入\n");
+}
+int main(int argc, const char * argv[])
+{
+	int option = 0;
+	LoadFromFile(FILENAME);//从文件读入题库
+	while (1)
+	{
+		printf("\n输入1\t录入新试题\n");
+		printf("输入2\t查看全部试题\n");
+		printf("输入3\t追加\n");
+		printf("输入4\t组题\n");
+		printf("输入5\t答题\n");
+		scanf("%d", &option);
+		printf("\n");
+		switch (option)
+		{
+		case 1:
+			option1();
+			break;
+		case 2:
+			option2();
+			break;
+		case 3:
+			option3();
+			break;
+		case 4:
+			option4();
+			break;
+		case 5:
+			option5();
+			break;
+		default:
+			error();
+		}
+	}
 }
